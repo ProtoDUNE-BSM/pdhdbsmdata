35620368abb0945511ca3ebb64c6cbfb99cff9a8
Ciaran Hasnip <ciaran.hasnip@physics.ox.ac.uk>
2025-01-14 10:50:45 -0600

    Ready to run on grid with justin. Justin scripts written and master fcl file ready.

--- test/justin_pdhddata_command.sh
+++ test/justin_pdhddata_command.sh
@@ -2,32 +2,34 @@
 
 :<<'EOF'
 
-To use this jobscript to process 5 files from the dataset fardet-hd__fd_mc_2023a_reco2__full-reconstructed__v09_81_00d02__standard_reco2_dune10kt_nu_1x2x6__prodgenie_nu_dune10kt_1x2x6__out1__validation
-data and put the output in the $USER namespace (MetaCat) and saves the output in /scratch
-Use this command to create the workflow:
+Submit jobs with justin using metacat query to find np04 BSM trigger files
+
+make the tarball and upload it to cvmfs by running:
+
+source srcs/pdhdbsmdata/test/tarball_protodunedm.sh
+htgettoken -a htvaultprod.fnal.gov -i dune
+INPUT_TAR_DIR_LOCAL=`justin-cvmfs-upload LocalProdNeutrinoNP04.Blob.tar.gz`
 
 EOF
 
 WORKDIR=${PWD}
 USERF=${USER}/ProtoDUNEBSM/PDHDBSMData
 FNALURL='https://fndcadoor.fnal.gov:2880/dune/scratch/users'
-MQL_QUERY="files from dune:all where core.runs in (29424) and core.run_type=hd-protodune and core.data_tier=raw and core.file_type=detector limit 1"
+MQL_QUERY="files from dune:all where core.runs in (29425) and core.run_type=hd-protodune and core.data_tier=raw and core.file_type=detector limit 1"
 
 echo "Setting INPUT_TAR_DIR_LOCAL = $INPUT_TAR_DIR_LOCAL"
 
-#justin simple-workflow --mql "${MQL_QUERY}" \
-#  --env INPUT_TAR_DIR_LOCAL="$INPUT_TAR_DIR_LOCAL" \
-#  --jobscript ${WORKDIR}/srcs/pdhdbsmdata/test/submit_pdhdbsm_jobscript.jobscript \
-#  --rss-mb 8000 --env NUM_EVENTS=1 \
-#  --scope usertests --lifetime-days 1 \
-#  --output-pattern "*_pdhdreco2_*.root:${FNALURL}/${USERF}"
+justin simple-workflow --mql "${MQL_QUERY}" \
+  --env INPUT_TAR_DIR_LOCAL="$INPUT_TAR_DIR_LOCAL" \
+  --jobscript ${WORKDIR}/srcs/pdhdbsmdata/test/submit_pdhdbsm_jobscript.jobscript \
+  --rss-mb 8000 --env SPS_DATA="spillrun029425.csv" \
+  --scope usertests --lifetime-days 1 \
+  --output-pattern "*_pdhdreco2_*.root:${FNALURL}/${USERF}"
 
+:<<'EOF'
 justin-test-jobscript --mql "${MQL_QUERY}" \
   --env INPUT_TAR_DIR_LOCAL="$INPUT_TAR_DIR_LOCAL" \
   --jobscript ${WORKDIR}/srcs/pdhdbsmdata/test/submit_pdhdbsm_jobscript.jobscript \
-  --env NUM_EVENTS=1 \
-#justin-test-jobscript \
-#  --mql ${MQL_QUERY} \
-#  --jobscript ${WORKDIR}/srcs/pdhdbsmdata/test/submit_pdhdbsm_jobscript.jobscript \
-#  --env NUM_EVENTS=1
-  #--output-pattern '*_pdhdreco2_*.root:$FNALURL/$USERF' 
+  --env SPS_DATA="spillrun029425.csv" \
+  #--env NUM_EVENTS=1
+EOF
--- test/tarball_protodunedm.sh
+++ test/tarball_protodunedm.sh
@@ -2,10 +2,8 @@
 
 # Bash script to make the tarball that is given to the FermiGrid production job
 
-DMWORKDIR=/exp/dune/app/users/chasnip/CERN_Fellowship/protodunedm_mc_simulation
-LOCALPROD=localProducts_larsoft_v10_01_03_e26_prof
-
-LOCALPRODDIR=${DMWORKDIR}/${LOCALPROD}
+DMWORKDIR=${PWD}
+LOCALPRODDIR=${DMWORKDIR}/localProducts_*
 
 if [ -z "${LOCALPRODDIR}" ]; then
   echo "[ERROR]: not source localproducts is not set up, cannot tar up required binaries."
@@ -13,17 +11,16 @@ if [ -z "${LOCALPRODDIR}" ]; then
 fi
 
 mkdir tar_state; cd tar_state
-mkdir protodunedm_mc_simulation
-cp -r ${LOCALPRODDIR} ./protodunedm_mc_simulation/
-mkdir protodunedm_mc_simulation/srcs
-cp -r ${DMWORKDIR}/srcs/pdhdbsmsimulation ./protodunedm_mc_simulation/srcs
-cp -r ${DMWORKDIR}/srcs/pdhdbsmdata ./protodunedm_mc_simulation/srcs
-cp -r ${DMWORKDIR}/srcs/dunetrigger ./protodunedm_mc_simulation/srcs
+mkdir protodunedm
+cp -r ${LOCALPRODDIR} ./protodunedm/
+mkdir protodunedm/srcs
+#cp -r ${DMWORKDIR}/srcs/pdhdbsmsimulation ./protodunedm/srcs
+cp -r ${DMWORKDIR}/srcs/pdhdbsmdata ./protodunedm/srcs
 
-tar --exclude '.git' -zcvf LocalProdNumu.Blob.tar.gz ./*
+tar --exclude '.git' -zcvf LocalProdNeutrinoNP04.Blob.tar.gz ./*
 
 cd ..
 
-mv tar_state/LocalProdNumu.Blob.tar.gz .
+mv tar_state/LocalProdNeutrinoNP04.Blob.tar.gz .
 rm -rf tar_state
 
